fd38c5c091d7ad4fe3d624eab76b7e75a593fb8b
Inwerwm <saclainvervm@gmail.com>
2021-04-20 18:42:20 +0900

    コミットを行うクラスを作成

---
+++ CommitPMX/CommitPMX/Comitter.cs
@@ -0,0 +1,27 @@
+﻿using PEPlugin.Pmx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommitPMX
+{
+    class Comitter
+    {
+        public void Commit(IPXPmx model,string comment)
+        {
+
+        }
+
+        public void WriteLog(string comment)
+        {
+
+        }
+
+        public void WriteModel(IPXPmx model, string comment)
+        {
+
+        }
+    }
+}
