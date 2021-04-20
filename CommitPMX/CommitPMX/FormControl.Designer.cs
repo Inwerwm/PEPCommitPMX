145f4640da27ce236c04e094b17170c432065840
Inwerwm <saclainvervm@gmail.com>
2021-04-20 18:11:41 +0900

    フォームコントロールの追加に必要な要素の生成

--- CommitPMX/CommitPMX/FormControl.Designer.cs
+++ CommitPMX/CommitPMX/FormControl.Designer.cs
@@ -29,10 +29,17 @@ namespace CommitPMX
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "FormControl";
             this.Text = "CommitPMX";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
