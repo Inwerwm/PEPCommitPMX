a117f852e51a1d06742883bafabb04198fa1b581
Inwerwm <saclainvervm@gmail.com>
2021-05-07 00:55:21 +0900

    標準ライブラリによる圧縮方法クラスを追加

---
+++ CommitPMX/CommitPMX/Compressor/StandardCompressor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommitPMX.Compressor
+{
+    /// <summary>
+    /// .NET 標準ライブラリによる圧縮
+    /// </summary>
+    class StandardCompressor : ICompressor
+    {
+        public void AddFileToArchive(string filePath, string archivePath)
+        {
+            using (var archive = ZipFile.Open(archivePath, File.Exists(archivePath) ? ZipArchiveMode.Update : ZipArchiveMode.Create))
+                archive.CreateEntryFromFile(filePath, Path.GetFileName(filePath), CompressionLevel.Optimal);
+        }
+    }
+}
