0898a3dd3f501cb555d97c7bec530b723e72512f
Inwerwm <saclainvervm@gmail.com>
2021-05-18 18:04:05 +0900

    CSV読込処理のための部分を削除

---
+++ CommitPMX/CommitPMX/CommitLog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommitPMX
+{
+    struct CommitLog
+    {
+        public DateTime Date { get; }
+        public string Message { get; }
+        public string FileName { get; }
+        public string ArchiveFormat { get; }
+        public string SavedPath { get; }
+
+        public CommitLog(DateTime date, string message, string fileName, string archiveFormat, string savedPath)
+        {
+            Date = date;
+            Message = message;
+            FileName = fileName;
+            ArchiveFormat = archiveFormat;
+            SavedPath = savedPath;
+        }
+    }
+}
--- CommitPMX/CommitPMX/FormReconstruction.cs
+++ CommitPMX/CommitPMX/FormReconstruction.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.VisualBasic.FileIO;
-using PEPlugin;
+﻿using PEPlugin;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -30,26 +29,6 @@ namespace CommitPMX
         {
             var commitDir = Commit.BuildCommitDirectryPath(Args.Host.Connector.Pmx.CurrentPath);
 
-            using (TextFieldParser parser = new TextFieldParser(Path.Combine(commitDir, Commit.LogCSVName)))
-            {
-                parser.TextFieldType = FieldType.Delimited;
-                parser.SetDelimiters(new string[] { "," });
-                parser.CommentTokens = new string[] { "#" };
-                parser.HasFieldsEnclosedInQuotes = true;
-                parser.TrimWhiteSpace = false;
-
-                var header = parser.ReadFields();
-                foreach (var col in header)
-                {
-                    dataGridViewCommits.Columns.Add(col, col);
-                }
-
-                while (!parser.EndOfData)
-                {
-                    var currentRow = parser.ReadFields();
-                    dataGridViewCommits.Rows.Add(currentRow);
-                }
-            }
         }
     }
 }
