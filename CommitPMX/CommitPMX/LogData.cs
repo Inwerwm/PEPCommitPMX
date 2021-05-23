6ae31be8b70a329a493ed5754243a16b598af63f
Inwerwm <saclainvervm@gmail.com>
2021-05-24 00:23:03 +0900

    履歴データ用オブジェクトを作成

---
+++ CommitPMX/CommitPMX/LogData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommitPMX
+{
+    class LogData
+    {
+        public string Directory { get; }
+        public string FilePath { get; }
+
+    }
+}
