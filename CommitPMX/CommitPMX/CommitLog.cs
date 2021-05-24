3101352dd0cb215fe1e1fef1582c920fe7ad6af0
Inwerwm <saclainvervm@gmail.com>
2021-05-24 16:34:53 +0900

    ファイル名をフォーマッタで指定するコンストラクタを追加

--- CommitPMX/CommitPMX/CommitLog.cs
+++ CommitPMX/CommitPMX/CommitLog.cs
@@ -22,6 +22,16 @@ namespace CommitPMX
             SavedPath = savedPath;
         }
 
+        public CommitLog(DateTime date, string message, Func<DateTime, string, string> filenameFormatter, ArchiveFormat format, string savedPath)
+        {
+            Date = date;
+            Message = message;
+            Filename = filenameFormatter(date, message);
+            Format = format;
+            SavedPath = savedPath;
+        }
+
+
         public string ToJson() => JsonConvert.SerializeObject(this, Formatting.None);
         public static CommitLog FromJson(string json) => JsonConvert.DeserializeObject<CommitLog>(json);
 
