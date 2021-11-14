56151d0000429c255c44ee7429b99baea2e6fa14
Inwerwm <saclainvervm@gmail.com>
2021-11-14 13:31:59 +0900

    誤字修正

--- CommitPMX/batch/Create-GitHubRelease.ps1
+++ CommitPMX/batch/Create-GitHubRelease.ps1
@@ -2,6 +2,6 @@ if (!(Test-Path Licenses)) {
     New-Item -Path Licenses -ItemType Directory
 }
 Copy-Item -Path ..\..\..\..\LICENSE  -Destination Licenses/CommitPMX
-Copy-Item -Path ..\..\..\batch\Licenses\SevenZipSharp  -Destinatio Licenses/SevenZipSharp
+Copy-Item -Path ..\..\..\batch\Licenses\SevenZipSharp  -Destination Licenses/SevenZipSharp
 Copy-Item -Path ..\..\..\batch\Licenses\7-Zip  -Destination Licenses/7-Zip
 Compress-Archive -Path 7z.cdll, CommitPMX.dll, CommitPMX.dll.config, Newtonsoft.Json.dll, PEPExtensions.dll, SevenZipSharp.dll, ..\..\..\..\README.html, ..\..\..\batch\ê‡ñæèë.html, Licenses -DestinationPath release.zip -Force
